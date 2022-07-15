084a495a5a751023869be8c5ea5b90760529853c
Mohr, Dennis <dennis.m.mohr@gmail.com>
2022-07-15 15:22:32 -0600

    Adding simple AI
    - Set player 2 to automatic selection
    - Created random number generator to pick a number from 1 - 9

--- Program.cs
+++ Program.cs
@@ -16,36 +16,46 @@ namespace TicTacToe
 
             // Continue to allow each player to select squares until a player wins or a draw occurs with all spaces selected
             while(complete == 0) {
+                
                 Console.Clear();  // Clear the board after each turn
                 Console.WriteLine("Player " + player + " turn:"); // Identify which player's turn it is
 
                 // Generate a simple board
                 Console.WriteLine("--------------------------");
-                Console.WriteLine("|  "+spaces[0]+"    |    "+spaces[1]+"   |   "+spaces[2]+"   |");
+                Console.WriteLine("|   "+spaces[0]+"   |    "+spaces[1]+"   |   "+spaces[2]+"   |");
                 Console.WriteLine("--------------------------");
                 Console.WriteLine("|   "+spaces[3]+"   |    "+spaces[4]+"   |   "+spaces[5]+"   |");
                 Console.WriteLine("--------------------------");
                 Console.WriteLine("|   "+spaces[6]+"   |    "+spaces[7]+"   |   "+spaces[8]+"   |");
                 Console.WriteLine("--------------------------");
 
-                // Prompt user for input
-                Console.WriteLine("Please input a number between 1 and 9:");
-
-                // Catch number exception
-                try{
-                    selected = int.Parse(Console.ReadLine());
-                }
-                catch(Exception e)
+                if(player == 2)
                 {
-                    Console.WriteLine("Number exception: " + e.Message);                    
-                    Console.WriteLine("Please input a number between 1 and 9:");
-                    selected = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Auto generate keystroke for player 2");
+                    selected = Player2(spaces);
+                    Thread.Sleep(500);
                 }
-
-                // Verify that the users selection is in the range of spaces available
-                while( selected < 1 || selected > spaces.Length) {
+                else
+                {
+                    // Prompt user for input
                     Console.WriteLine("Please input a number between 1 and 9:");
-                    selected = int.Parse(Console.ReadLine());
+
+                    // Catch number exception
+                    try{
+                        selected = int.Parse(Console.ReadLine());
+                    }
+                    catch(Exception e)
+                    {
+                        Console.WriteLine("Number exception: " + e.Message);                    
+                        Console.WriteLine("Please input a number between 1 and 9:");
+                        selected = int.Parse(Console.ReadLine());
+                    }
+
+                    // Verify that the users selection is in the range of spaces available
+                    while( selected < 1 || selected > spaces.Length) {
+                        Console.WriteLine("Please input a number between 1 and 9:");
+                        selected = int.Parse(Console.ReadLine());
+                    }
                 }
 
                 // Provide feedback for the player selection
@@ -63,7 +73,7 @@ namespace TicTacToe
                         }
                     } else {
                         spaces[selected-1] = "O";
-                        if(Complete(spaces,"O",1) != player)
+                        if(Complete(spaces,"O",2) != player)
                         {
                             player--;
                         } else {
@@ -73,7 +83,7 @@ namespace TicTacToe
                 }
 
                 // Check for a draw
-                if(Complete(spaces,"X",1) == -1) {
+                if(IsDraw(spaces)) {
                     complete = -1;
                 }
 
@@ -81,11 +91,54 @@ namespace TicTacToe
            
             // The game is complete
             if(complete != -1) { // Display the winner
-                Console.WriteLine("The winner is: " + complete + "!");
+                Console.WriteLine("The winner is player: " + complete + "!");
             } else { // Display Draw message
                 Console.WriteLine("The game was a DRAW!");
             }
     
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("|  "+spaces[0]+"    |    "+spaces[1]+"   |   "+spaces[2]+"   |");
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("|   "+spaces[3]+"   |    "+spaces[4]+"   |   "+spaces[5]+"   |");
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("|   "+spaces[6]+"   |    "+spaces[7]+"   |   "+spaces[8]+"   |");
+            Console.WriteLine("--------------------------");
+
+
+        }
+
+        private static int Player2(string[] spaces) {
+
+            Random rand = new Random();
+            int i = rand.Next(1,9);
+
+            while(spaces[i] == "X" && spaces[i] == "O") {
+                // Generate a different number
+                i = rand.Next(1,9);
+            }
+
+            Console.WriteLine("player 2 selection = " + i);
+
+            return i;
+        }
+
+        private static bool IsDraw(string[] spaces) {
+
+            bool isDraw = false;
+            int count = 0;
+
+            // Check to see if the game was a draw
+            for(int i = 0; i < spaces.Length; i++) {
+                if(spaces[i] != (i+1).ToString()){
+                    count++;   
+                }
+            }
+ 
+            if(count == spaces.Length) {
+                isDraw = true;
+            }
+
+            return isDraw;
 
         }
 
@@ -93,7 +146,6 @@ namespace TicTacToe
         private static int Complete(string[] spaces, string lttr, int player) {
             
             int status = 0;
-            int count = 0;
 
             // Horizontal Row 1
             if(spaces[0] == lttr && spaces[1] == lttr && spaces[2] == lttr) {
@@ -126,17 +178,6 @@ namespace TicTacToe
             // Vertical Column 3
             if(spaces[2] == lttr && spaces[5] == lttr && spaces[8] == lttr) {
                 status = player;
-            }
-
-            // Check to see if the game was a draw
-            for(int i = 0; i < spaces.Length; i++) {
-                if(spaces[i] != (i+1).ToString()){
-                    count++;   
-                }
-            }
- 
-            if(count == spaces.Length) {
-                status = -1;
             }
 
             return status;
